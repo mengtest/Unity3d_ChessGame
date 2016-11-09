18dec53a129cf98e280651c91c0fb148bdd736e0
ortofone24 <ortofone24@gmail.com>
2016-11-09 17:25:10 +0100

    First Commit
    Addes Unity Project to GitHub

---
+++ Chess2.0/Assets/Scripts/Menu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas QuitMenu;
+    public Button ButtonStart;
+    public Button ButtonExit;
+
+    private Canvas menuUI;
+
+    private void Start()
+    {
+        menuUI = GetComponent<Canvas>();
+        QuitMenu = QuitMenu.GetComponent<Canvas>();
+        ButtonStart = ButtonStart.GetComponent<Button>();
+        ButtonExit = ButtonExit.GetComponent<Button>();
+
+        QuitMenu.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            menuUI.enabled = !menuUI.enabled;
+        }
+    }
+
+    public void ExitGameButton()
+    {
+        QuitMenu.enabled = true;
+        ButtonStart.enabled = false;
+        ButtonExit.enabled = false;
+    }
+
+    public void DontLeaveGameButton()
+    {
+        QuitMenu.enabled = false;
+        ButtonStart.enabled = true;
+        ButtonExit.enabled = true;
+    }
+
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene("ChessGame");
+    }
+
+    public void ExitGameButtonYes()
+    {
+        Application.Quit();
+    }
+}
